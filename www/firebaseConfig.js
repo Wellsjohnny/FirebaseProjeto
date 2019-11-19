a6ce65a7986f5282a9e2a67b3b4a43fd4a251997
Wellsjohnny <wmsswizard@gmail.com>
2019-11-19 10:32:37 -0300

    ProjetoFireBase

---
+++ www/firebaseConfig.js
@@ -0,0 +1,10 @@
+var fbConfig = {
+    apiKey: " AIzaSyBncqIRl-l37SCI7fLC702bScM4iPSKOyU ",
+    authDomain: "projetofirebaseapp-8f275.firebaseapp.com",
+    databaseURL: "https://projetofirebaseapp-8f275.firebaseio.com",
+    projectId: "projetofirebaseapp-8f275",
+    storageBucket: "projetofirebaseapp-8f275.appspot.com",
+    messagingSenderId: "673111214715",
+    appId: "1:673111214715:web:5efabfb4e7c13df028b0de",
+    measurementId: "G-KTNJHLWFJV"
+};
